b9049aebc2720c8f9c0799bf7cc4695bdd1de19b
Leandro Gonaçalves <contato.leandrogoncalves@gmail.com>
2020-06-22 19:31:14 -0300

    Deploy no Heroku

---
+++ build/precache-manifest.b29dd2e43fd2e32ffe801a1b0a852fee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ee258f0bced896d4b1f0b882de8ece",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72a23db613eaca117e51",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "835a11adf3fc95a85f28",
+    "url": "/static/css/main.81595025.chunk.css"
+  },
+  {
+    "revision": "72a23db613eaca117e51",
+    "url": "/static/js/2.a9116304.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.a9116304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "835a11adf3fc95a85f28",
+    "url": "/static/js/main.dcfb7798.chunk.js"
+  },
+  {
+    "revision": "e4ec640b76dbfc7cc28f",
+    "url": "/static/js/runtime-main.51284287.js"
+  }
+]);
